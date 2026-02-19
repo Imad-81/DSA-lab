001b3b593b3312a2fe028e72f1d935501951f742
Imad-81 <shaikimaduddin10@gmail.com>
2026-02-19 15:14:02 +0530

    code 50% done

--- 19-02_linkedlist.py
+++ 19-02_linkedlist.py
@@ -10,7 +10,22 @@ class LinkedList:
 
     def add_student(self, name, roll_number, marks):
         new_student = Node(name, roll_number, marks)
+        new_student.next = self.head
+        self.head = new_student
+
+    def display_student(self):
+        current = self.head
+        while current:
+            print(f"Name: {current.name}, Roll Number: {current.roll_number}, Marks: {current.marks}")
+
+            current = current.next
+
+    def calculate_cgpa(self):
+        total_marks = 0 
+        total_students = 0 
         
+        
+
 
 
         
