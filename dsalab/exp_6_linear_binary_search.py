226a22d21cbe43955483d833e3a831ba6c3bbc0b
imad-81 <shaikimaduddin10@gmail.com>
2026-04-02 09:21:09 +0000

    upadted code for exp 6

--- dsalab/exp_6_linear_binary_search.py
+++ dsalab/exp_6_linear_binary_search.py
@@ -1,54 +1,24 @@
 class Product:
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
+    def __init__(self, n, p): self.name, self.price = n, p
 
-def linear_search(dataset, target):
+def linear_search(d, t):
+    for i, x in enumerate(d):
+        if x.name == t: return i
+    return -1
 
-    for i, product in enumerate(dataset):
-        if product.name == target:
-            return i  # Return the index if found
-    return -1  # Return -1 if not found
+def binary_search(d, t):
+    l, h = 0, len(d) - 1
+    while l <= h:
+        m = (l + h) // 2
+        if d[m].name == t: return m
+        if d[m].name < t: l = m + 1
+        else: h = m - 1
+    return -1
 
-def binary_search(dataset, target):
-    """
-    Binary search algorithm to find a product in the sorted dataset.
-    """
-    low, high = 0, len(dataset) - 1
-    while low <= high:
-        mid = (low + high) // 2
-        mid_product = dataset[mid].name
-        if mid_product == target:
-            return mid  # Return the index if found
-        elif mid_product < target:
-            low = mid + 1
-        else:
-            high = mid - 1
-    return -1  # Return -1 if not found
+dataset = [Product(f'Product{i}', p) for i, p in enumerate([10.99, 24.99, 34.99, 14.99, 19.99], 1)]
+target = 'Product3'
 
-# Sample dataset (assuming it's a list of Product instances)
-dataset = [
-    Product('Product1', 10.99),
-    Product('Product2', 24.99),
-    Product('Product3', 34.99),
-    Product('Product4', 14.99),
-    Product('Product5', 19.99),
-]
-
-# Perform linear search
-target_product = 'Product3'
-linear_result = linear_search(dataset, target_product)
-if linear_result != -1:
-    print(f"Linear Search: {target_product} found at index {linear_result}")
-else:
-    print(f"Linear Search: {target_product} not found in the dataset")
-
-# Sort dataset for binary search (assuming the 'name' field is sortable)
-dataset.sort(key=lambda x: x.name)
-
-# Perform binary search
-binary_result = binary_search(dataset, target_product)
-if binary_result != -1:
-    print(f"Binary Search: {target_product} found at index {binary_result}")
-else:
-    print(f"Binary Search: {target_product} not found in the dataset")
+for name, search_func in [("Linear", linear_search), ("Binary", binary_search)]:
+    if name == "Binary": dataset.sort(key=lambda x: x.name)
+    res = search_func(dataset, target)
+    print(f"{name} Search: {target} {'found at index ' + str(res) if res != -1 else 'not found'}")
